d96e73c07db973b869d6c89fb868c69c7168c5bb
Jacob Young <youngjacob2002@gmail.com>
2023-06-17 18:04:07 -0700

    app skeleton

--- app.py
+++ app.py
@@ -4,20 +4,25 @@ import os
 
 app = Flask(__name__)
 
-grocery_list = []
+queries = []
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        item = request.form.get('item')
-        grocery_list.append(item)
-    if 'pdf' in request.files:
+        if 'submit_item' in request.form:
+            item = request.form.get('item')
+            queries.append(item)
+        elif 'submit_all' in request.form:
+            #run the API method
+            print("here")
+        elif 'pdf' in request.files:
             pdf_file = request.files['pdf']
             if pdf_file.filename != '':
                 filename = secure_filename(pdf_file.filename)
                 pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     
-    return render_template('index.html', grocery_list=grocery_list)
+    
+    return render_template('index.html', grocery_list=queries)
 
 if __name__ == '__main__':
     app.run()
